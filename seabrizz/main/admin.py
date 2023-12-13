b57d4aad4ee52f0673abaf8f4ce1626ff7e59fff
MakarSux <mm.pyshkin@yandex.ru>
2023-12-13 11:54:48 +0300

    создал модель

--- seabrizz/main/admin.py
+++ seabrizz/main/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import SeaFood
 
+
+admin.site.register(SeaFood)
 # Register your models here.
--- seabrizz/main/models.py
+++ seabrizz/main/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+
+class SeaFood(models.Model):
+    name = models.CharField('Наименование', max_length=75)
+    proiz = models.CharField('Производиьтель', max_length= 75)
+    price = models.CharField('Цена', max_length=75)
+    descr = models.TextField('Описание')
+    image = models.ImageField(upload_to='images/')
+    quantity = models.IntegerField('Количество', default=0)
+
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
 # Create your models here.
